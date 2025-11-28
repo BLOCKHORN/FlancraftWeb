58661017b84e15c7e6f4ad43f057a02ef928ce26
programaestanco <programaestanco@gmail.com>
2025-11-28 13:54:24 +0100

    ritual ECO cambiado

--- apps/frontend/src/components/Landpage/SectionDivider.jsx
+++ apps/frontend/src/components/Landpage/SectionDivider.jsx
@@ -1,9 +1,17 @@
-// src/components/SectionDivider.jsx
-import React from 'react';
-import '../../styles/components/Landpage/_sectiondivider.scss';
+// src/components/Landpage/SectionDivider.jsx
+import React from "react";
+import "../../styles/components/Landpage/_sectiondivider.scss";
 
 const SectionDivider = () => {
-  return <div className="section-divider" />;
+  return (
+    <div className="section-divider" aria-hidden="true">
+      <img
+        className="section-divider__image"
+        src="/assets/Divider.png"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default SectionDivider;
